dc545754b0e5c6e0bfe53acf9c7cb8bc997de68d
Max Ning <maxning24@gmail.com>
2020-11-05 21:37:30 -0500

    winner output

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -36,8 +36,15 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner' + winner;
+      }
+      else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
